7e878e67d56fe9d271211b482f49a4286b053858
Arceen <niloyman01@gmail.com>
2025-08-05 21:08:40 +0600

    db setup & seaorm testing

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,18 @@
+use sea_orm::{ConnectOptions, Database};
+use std::env;
+use std::time::Duration;
+
 #[tokio::main]
-async fn main() {}
+async fn main() {
+    dotenvy::dotenv().ok();
+    let db_uri = env::var("DB_URI").expect("Could not find the matching env");
+    println!("db_url: {db_uri}");
+    let mut opt = ConnectOptions::new(db_uri);
+    opt.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(8))
+        .sqlx_logging(true)
+        .sqlx_logging_level(log::LevelFilter::Info)
+        .set_schema_search_path("public");
+    let db = Database::connect(opt).await.unwrap();
+}
