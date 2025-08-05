13c723f4b7edc33092bf57de214856fd84c005bb
Arceen <niloyman01@gmail.com>
2025-08-05 19:44:01 +0600

    websocket research deepens

--- src/bin/broker.rs
+++ src/bin/broker.rs
@@ -1,15 +1,18 @@
-use futures_util::stream::StreamExt;
-use futures_util::{future, TryStreamExt};
-use tokio_tungstenite::accept_async;
+use futures_util::{SinkExt, StreamExt};
 
 #[tokio::main]
 async fn main() {
-    println!("Hello broker!");
-    let stream = tokio::net::TcpListener::bind(("0.0.0.0", 8081)).await.unwrap();
-    let (stream, _) = stream.accept().await.unwrap();
-    let addr = stream.peer_addr().expect("something else");
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
 
-    let ws_stream = accept_async(stream).await.expect("you should have known!");
-    let (write, read) = ws_stream.split();
-    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary())).forward(write).await.unwrap();
+    while let Ok((stream_sock, _)) = listener.accept().await {
+        println!("Socket received from: {}", stream_sock.peer_addr().unwrap());
+        let _ = tokio::spawn(async move {
+            let mut ws_socket = tokio_tungstenite::accept_async(stream_sock).await.unwrap();
+            while let Some(msg) = ws_socket.next().await {
+                ws_socket.send(msg.unwrap()).await;
+            }
+        }).await;
+    }
 }
--- src/bin/worker.rs
+++ src/bin/worker.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello worker");
+use futures_util::{SinkExt, StreamExt};
+use tokio_tungstenite::tungstenite::client::IntoClientRequest;
+use tokio_tungstenite::tungstenite::Message;
+
+#[tokio::main]
+async fn main() {
+    let request = "ws://127.0.0.1:8080".into_client_request().unwrap();
+    if let Ok((stream, response)) = tokio_tungstenite::connect_async(request).await {
+        let _ = tokio::spawn(async move {
+            let (mut write, read) = stream.split();
+            let read_handler = tokio::spawn(async move {
+                read.for_each(|msg|
+                    async {
+                        let data = msg.unwrap().into_data();
+                        println!("{}", String::from_utf8(data.to_vec()).unwrap());
+                    }).await;
+            });
+            let write_handler = tokio::spawn(async move {
+                let mut i = 0_u32;
+                loop {
+                    println!("sending stuff!");
+                    let _ = write.send(Message::Text(format!("{}\n", i).into())).await;
+                    tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+                    i += 1;
+                }
+            });
+            let _ = tokio::join!(read_handler, write_handler);
+        }).await.expect("could not await client thread");
+    }
 }
