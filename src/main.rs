5662d3ff99a2df0487c5a4269d03ef60d57f871f
Arceen <niloyman01@gmail.com>
2025-08-06 20:40:19 +0600

    dependency injection test

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,56 @@
-use sea_orm::{ConnectOptions, Database};
-use std::env;
-use std::time::Duration;
+use std::collections::HashMap;
 
-#[tokio::main]
-async fn main() {
-    dotenvy::dotenv().ok();
-    let db_uri = env::var("DB_URI").expect("Could not find the matching env");
-    println!("db_url: {db_uri}");
-    let mut opt = ConnectOptions::new(db_uri);
-    opt.max_connections(100)
-        .min_connections(5)
-        .connect_timeout(Duration::from_secs(8))
-        .sqlx_logging(true)
-        .sqlx_logging_level(log::LevelFilter::Info)
-        .set_schema_search_path("public");
-    let db = Database::connect(opt).await.unwrap();
+#[derive(Debug, Clone)]
+struct User {
+    id: u16,
+    name: String,
+    email: String,
+}
+
+trait UserRepository {
+    fn find_by_id(&self, id: u16) -> Option<User>;
+    fn save(&mut self, user: User) -> Result<(), String>;
+    fn find_by_email(&self, email: String) -> Option<User>;
+}
+
+struct InMemoryRepository {
+    db: HashMap<u16, User>,
+    next_id: u16,
+}
+
+impl InMemoryRepository{
+    fn new()->Self {
+        Self {
+            db: HashMap::new(),
+            next_id: 1,
+        }
+    }
+}
+impl UserRepository for InMemoryRepository {
+    fn find_by_id(&self, id: u16) -> Option<User> {
+        self.db.get(&id).cloned()
+    }
+
+    fn find_by_email(&self, email: String) -> Option<User> {
+        self.db.values().find(|&user| user.email == email).cloned()
+    }
+
+    fn save(&mut self, mut user: User) -> Result<(), String> {
+        if user.id == 0 {
+            user.id = self.next_id;
+            self.next_id += 1;
+        }
+        self.db.insert(self.next_id, user);
+        Ok(())
+    }
+}
+
+fn main() {
+    let mut mem_repo = InMemoryRepository::new();
+    mem_repo.save(User{id: 0, name: "Jamal".into(), email: "jamal@gmail.com".into()}).unwrap();
+    mem_repo.save(User{id: 0, name: "Kano".into(), email: "kano@gmail.com".into()}).unwrap();
+    mem_repo.save(User{id: 0, name: "Al".into(), email: "al@gmail.com".into()}).unwrap();
+    mem_repo.save(User{id: 0, name: "Oppa".into(), email: "oppa@gmail.com".into()}).unwrap();
+    println!("{:#?}", mem_repo.find_by_email("al@gmail.com".into()).unwrap());
+    println!("{:#?}", mem_repo.find_by_id(2).unwrap());
 }
