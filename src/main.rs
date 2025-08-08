fe24d30c55116f5b417e8cfdef7b6b60313db81b
Arceen <niloyman01@gmail.com>
2025-08-08 23:58:28 +0600

    mandelbrot set cmd line

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,68 @@
-use std::mem;
+use num::complex::Complex;
 
-enum MyEnum {
-    A { name: String, x: u8 },
-    B { name: String },
+fn calculate_mandelbrot(
+    max_iters: usize,
+    x_min: f64,
+    x_max: f64,
+    y_min: f64,
+    y_max: f64,
+    width: usize,
+    height: usize,
+) -> Vec<Vec<usize>> {
+    let mut rows: Vec<_> = Vec::with_capacity(width);
+    for img_y in 0..height {
+        let mut row: Vec<usize> = Vec::with_capacity(height);
+        for img_x in 0..width {
+            let x_percent = (img_x as f64 / width as f64);
+            let y_percent = (img_y as f64/ height as f64);
+            let cx = x_min + (x_max - x_min) * x_percent;
+            let cy = y_min + (y_max - y_min) * y_percent;
+            let escaped_at = mandelbrot_at_point(cx, cy, max_iters);
+            row.push(escaped_at);
+        }
+        rows.push(row);
+    }
+    rows
 }
 
-fn a_to_b(e: &mut MyEnum) {
-    if let MyEnum::A { name, x: 0 } = e {
-        *e = MyEnum::B {
-            name: mem::take(name),
+fn mandelbrot_at_point(
+    cx: f64,
+    cy: f64,
+    max_iters: usize,
+) -> usize {
+    let mut z = Complex { re: 0.0, im: 0.0};
+    let c = Complex::new(cx, cy);
+    for i in 0..max_iters {
+        if z.norm() > 2.0 {
+            return i;
         }
+        z = z * z + c
+    }
+    max_iters
+}
+
+fn render_mandelbrot(escape_vals: Vec<Vec<usize>>) {
+    for row in escape_vals {
+        let mut line = String::with_capacity(row.len());
+        for col in row {
+            let val = match col {
+                0..3 => ' ',
+                3..6 => '.',
+                6..11 => '•',
+                11..31 => '*',
+                31..101 => '+',
+                101..201 => 'x',
+                201..401 => '$',
+                401..701 => '#',
+                _ => '%',
+            };
+            line.push(val);
+        }
+        println!("{}", line);
     }
 }
-fn main() {
 
+fn main() {
+    let mandelbrot = calculate_mandelbrot(1000, -2.0, 1.0, -1.8, 1.8, 150, 80);
+    render_mandelbrot(mandelbrot);
 }
